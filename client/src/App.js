75edaa85167ead98253bca177c2a79c1e7e096ac
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-09-06 09:17:45 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
